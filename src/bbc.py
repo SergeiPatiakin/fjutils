2e86c3a3d695cbd63b21a50ddb18cc32db203c66
Sergei Patiakin <sergei.patiakin@gmail.com>
2018-01-06 12:45:40 +0100

    Add bitbucket script

---
+++ src/bbc.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Create a bitbucket repo and add it as a remote"""
+import sys
+import argparse
+import getpass
+from fj.bitbucket import bitbucket_create
+from fj.fjdb import load
+from fj.git import call_git
+
+parser = argparse.ArgumentParser("bbc")
+parser.add_argument('reponame')
+
+def main(argv):
+    args = parser.parse_args(argv)
+    reponame = args.reponame
+    fjutils_conf=load("fjutils", default_type='dict')
+    bitbucket_username = fjutils_conf.get('bitbucket_username')
+    bitbucket_password = fjutils_conf.get('bitbucket_password')
+    if not bitbucket_username:
+        print("Bitbucket credentials not found in config.")
+        bitbucket_username = input("Bitbucket username: ")
+    if not bitbucket_password:
+        bitbucket_password = getpass.getpass('Bitbucket password: ')
+    bitbucket_create(bitbucket_username, reponame, bitbucket_password)
+    call_git(['remote', 'add', 'bitbucket', 'https://{0}@bitbucket.org/{0}/{1}.git'.format(bitbucket_username, reponame)])
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ src/fj/bitbucket.py
@@ -0,0 +1,18 @@
+import http.client
+from base64 import b64encode
+
+def bitbucket_create(username, reponame, password):
+    """Create a bitbucket repository"""
+    # This sets up the https connection
+    conn = http.client.HTTPSConnection("api.bitbucket.org")
+    # we need to base 64 encode it, then decode to ascii
+    user_and_pass = b64encode(bytes("{0}:{1}".format(username, password), encoding='ISO-8859-1')).decode("ascii")
+    headers = {
+        'Authorization': 'Basic %s' %  user_and_pass,
+        'Content-Type': 'application/json',
+    }
+    body = '{"scm": "git", "is_private": true}'
+    # conn.request('GET', '/2.0/repositories/{0}/{1}'.format(username, reponame), headers=headers)
+    conn.request('POST', '/2.0/repositories/{0}/{1}'.format(username, reponame), headers=headers, body=body)
+    response = conn.getresponse()
+    assert response.status == 200, response.read()
